6dab57b7ad055abef715536936a8ae7a6bfac997
Ricky Waldron <ricky_waldron@hotmail.com>
2017-12-13 10:18:15 +0100

    finalVersionBlogApplication

--- allMessages.js
+++ allMessages.js
@@ -12,50 +12,62 @@ module.exports =
                 let data = []
                 data.push(result)
                 const query = {
-                text: `SELECT * from comments`
+                text: `SELECT commentTable.commentUser, commentTable.body, commentTable.message_id FROM(SELECT comments.body, comments.message_id, users.username AS commentUser 
+                        FROM comments LEFT JOIN users ON comments.user_id=users.user_id) AS commentTable`
                 }
                 client.query(query, (err, result) => {
                     if (err) throw (err)
                     let comments = []
                     comments.push(result)
-                    console.log(comments)
-                res.render('allMessages', { data: data, comments: comments })      
-                })
+                    res.render('allMessages', { data: data, comments: comments })    
             })
+        })
     }) 
 
         app.post('/addComment', (req, result) => {
             console.log("reached")
             if(req.session.user) {
             let userName = req.session.user
-            let theBody  = req.body.bodyComment
-            let messageId = req.body.hiddenMessageId
-            let userId = req.body.hiddenUserId
-            console.log("userId:", userId + "messageId", messageId, "comment: ", theBody)
-            const query3 = {
-            text: (`INSERT INTO comments (body, message_id, user_id) 
-                    VALUES ('${theBody}', (SELECT(message_id) FROM messages WHERE message_id= '${messageId}'), (
-                    SELECT (user_id) FROM users WHERE user_id= '${userId}')) RETURNING *`)
-        }
-        client.query(query3, (err, res) => {
-            if (err) throw err
-            console.log("the adding comment result: ", res.rows)
-        
-        const query4 = {
-            text: (`SELECT body FROM comments WHERE message_id='${messageId}'`)
-        }
-        client.query(query4, (err, res) => {
-            if (err) throw err
-            let newComment = []
-            newComment.push(res)
-            console.log(newComment)
-            result.render('allMessages', {newComment: newComment})   
+            let theBody  = req.body.theBody
+            let messageId = req.body.postId
+            let userId = 0
+            console.log("messageId", messageId, "comment: ", theBody)
+            const query1 = {
+                text: (`SELECT user_id FROM users WHERE username= '${userName}'`)
+            }
+            client.query(query1, (err, res) => {
+                if (err) throw err   
+                userId = res.rows[0].user_id
+            
+                const query3 = {
+                text: (`INSERT INTO comments (body, message_id, user_id) 
+                        VALUES ('${theBody}', (SELECT(message_id) FROM messages WHERE message_id= '${messageId}'), (
+                        SELECT (user_id) FROM users WHERE user_id= '${userId}')) RETURNING *`)
+                }
+                client.query(query3, (err, res) => {
+                    if (err) throw err
+                    console.log("the adding comment result: ", res.rows)
+                
+                    const query4 = {
+                        text: (`SELECT * FROM comments WHERE message_id='${messageId}'`)
+                    }
+                    client.query(query4, (err, res) => {
+                        if (err) throw err
+                        let newComment = []
+                        for(var i=0; i < res.rows.length; i++){
+                            console.log(res.rows.length)
+                        console.log("Reached: deze comment ")
+                        newComment.push(res.rows)
+                        console.log(newComment + "dit is de nieuwe comment")
+                        result.end('allMessages', {data: newComment})
+                        } 
+                    })
+                })
             })
-        })
-    }
-        else {
+        }
+            else {
             result.send("You can only comment on a post when you are logged in")
         }
     })
-    }
+}
 
